87dd8a2d56a9a81a374919bc0564eb5063f801b4
Lucas Nóbrega <lucasnobrega.js@gmail.com>
2023-03-29 17:44:29 -0300

    Implementando a questao 1 e 2 da lista 01 de estrutura de dados 1

---
+++ EstruturaDeDados1/Listas/01/q1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*
+ Função : Calcular hipotenusa
+ Autor : Lucas Emanuel Nascimento Nóbrega Dias
+ Data : 29/03/2023
+ Observações:
+*/
+
+float calculate_hypotenuse(float a, float b)
+{
+    return sqrt(pow(a, 2) + pow(b, 2));
+}
+
+int main()
+{
+    float a, b;
+
+    printf("Por favor, informe o cateto A: ");
+    scanf("%f", &a);
+
+    printf("Por favor, informe o cateto B: ");
+    scanf("%f", &b);
+
+    float h = calculate_hypotenuse(a, b);
+
+    printf("A hipotenusa é: %f", h);
+
+    return 0;
+}
---
+++ EstruturaDeDados1/Listas/01/q2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*
+ Função : Calcular salário do professor
+ Autor : Lucas Emanuel Nascimento Nóbrega Dias
+ Data : 29/03/2023
+ Observações:
+*/
+
+float calculate_salary(
+    float value_per_hour,
+    float class_hours_per_month,
+    float INSS_discount)
+{
+    return (value_per_hour * class_hours_per_month) * ((100 - INSS_discount) / 100);
+}
+
+int main()
+{
+    float value_per_hour, class_hours_per_month, INSS_discount;
+
+    printf("Por favor, informe valor da hora de aula: R$ ");
+    scanf("%f", &value_per_hour);
+
+    printf("Por favor, informe a quantidade de horas de aula dadas no mês: ");
+    scanf("%f", &class_hours_per_month);
+
+    printf("Por favor, informe a porcentagem de desconto do INSS: ");
+    scanf("%f", &INSS_discount);
+
+    printf("A valor do salário do mês é: R$ %.2f", calculate_salary(value_per_hour, class_hours_per_month, INSS_discount));
+
+    return 0;
+}
