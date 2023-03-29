b3627fbb235c98d55f1da01922e9292f6a800ff6
Lucas Nóbrega <lucasnobrega.js@gmail.com>
2023-03-29 18:42:58 -0300

    Implementando a questao 8 da lista 01 de estrutura de dados 1

---
+++ EstruturaDeDados1/Listas/01/q8.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*
+ Função : Calcular área do círculo
+ Autor : Lucas Emanuel Nascimento Nóbrega Dias
+ Data : 29/03/2023
+ Observações:
+*/
+
+const float PI = 3.14159265;
+
+float calculate_circle_area(float radius)
+{
+    return 4 * PI * pow(radius, 2);
+}
+
+int main()
+{
+    float radius;
+
+    printf("Por favor, informe a radio do círculo: ");
+    scanf("%f", &radius);
+
+    printf("A área do cículo é: %f", calculate_circle_area(radius));
+
+    return 0;
+}
