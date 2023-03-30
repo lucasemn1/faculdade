5e81ccd19c206c86728770a3dd2cde2b3f940d49
Lucas Nóbrega <lucasnobrega.js@gmail.com>
2023-03-30 20:25:02 -0300

    Implementando a questao 17 da lista 01 de estrutura de dados 1

---
+++ EstruturaDeDados1/Listas/01/q17.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*
+ Função : Calcular arranjos
+ Autor : Lucas Emanuel Nascimento Nóbrega Dias
+ Data : 30/03/2023
+ Observações:
+*/
+
+int calculate_factorial(int number)
+{
+    if (number == 1)
+        return 1;
+
+    return number * calculate_factorial(number - 1);
+}
+
+int calculate_arrangements_quantity(int n, int r)
+{
+    return calculate_factorial(n) / calculate_factorial(n - r);
+}
+
+int main()
+{
+    int n, r;
+
+    printf("Informe N: ");
+    scanf("%i", &n);
+
+    printf("Informe R: ");
+    scanf("%i", &r);
+
+    printf("O número de arranjos é: %i", calculate_arrangements_quantity(n, r));
+}
