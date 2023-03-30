52155333149b4b5b9af8f98163dac44218ee895c
Lucas Nóbrega <lucasnobrega.js@gmail.com>
2023-03-30 17:54:25 -0300

    Implementando a questao 10 da lista 01 de estrutura de dados 1

---
+++ EstruturaDeDados1/Listas/01/q10.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ Função : Calcular velocidade média
+ Autor : Lucas Emanuel Nascimento Nóbrega Dias
+ Data : 30/03/2023
+ Observações:
+*/
+
+float calcular_velocidade_media(float espaco, float tempo)
+{
+    return espaco / tempo;
+}
+
+int main()
+{
+    float espaco, tempo;
+
+    printf("Informe o espaço (KM): ");
+    scanf("%f", &espaco);
+
+    printf("Informe o tempo (H): ");
+    scanf("%f", &tempo);
+
+    printf("A velocidade média é: %.2f KM/H", calcular_velocidade_media(espaco, tempo));
+}
