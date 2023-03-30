97201333a39c3d59c1ec7d666eadd81c57a02806
Lucas Nóbrega <lucasnobrega.js@gmail.com>
2023-03-30 20:59:19 -0300

    Implementando a questao 19 e 20 da lista 01 de estrutura de dados 1

---
+++ EstruturaDeDados1/Listas/01/q19.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*
+ Função : Redução do elemento radioativo
+ Autor : Lucas Emanuel Nascimento Nóbrega Dias
+ Data : 30/03/2023
+ Observações:
+*/
+
+int calculate_time_to_be_final_mass(float mass, int current_time, float final_mass)
+{
+    if (mass == final_mass)
+    {
+        return current_time;
+    }
+
+    return current_time + calculate_time_to_be_final_mass(mass / 2, current_time + 50, final_mass);
+}
+
+int main()
+{
+    float mass;
+    int time = 0;
+
+    printf("Informe a massa (g) do elemento: ");
+    scanf("%f", &mass);
+
+    printf("Vai levar %i segundos até ficar com 0.05 gramas", calculate_time_to_be_final_mass(mass, time, 0.05));
+}
---
+++ EstruturaDeDados1/Listas/01/q20.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+/*
+ Função : Redução do elemento radioativo
+ Autor : Lucas Emanuel Nascimento Nóbrega Dias
+ Data : 30/03/2023
+ Observações:
+*/
+
+int calculate_IMC(float weight, float height)
+{
+    return weight / pow(height, 2);
+}
+
+void get_IMC_status(float imc, char *str)
+{
+    if (imc < 18.5)
+    {
+        strcpy(str, "abaixo do peso");
+    }
+    else if (imc >= 18.5 && imc <= 24.9)
+    {
+        strcpy(str, "normal");
+    }
+    else if (imc >= 25 && imc <= 29.9)
+    {
+        strcpy(str, "acima do peso");
+    }
+    else
+    {
+        strcpy(str, "obeso");
+    }
+}
+
+int main()
+{
+    float weight, height;
+
+    printf("Informe o peso (KG): ");
+    scanf("%f", &weight);
+
+    printf("Informe a altura (M): ");
+    scanf("%f", &height);
+
+    float imc = calculate_IMC(weight, height);
+    char imc_status[100];
+    get_IMC_status(imc, imc_status);
+
+    printf("Seu IMC é: %.2f \n\n", imc);
+
+    printf("VALORES DE IMC \n");
+    printf("Abaixo do peso: menor que 18,5 \n");
+    printf("Normal: entre 18,5 e 24,9 \n");
+    printf("Acima do peso: entre 25 e 29,9 \n");
+    printf("Obeso: 30 ou mais \n\n");
+
+    printf("Dentro desses parâmetros, seu IMC equivale a %s", imc_status);
+}
